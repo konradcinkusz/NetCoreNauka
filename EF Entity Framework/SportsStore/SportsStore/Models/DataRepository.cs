d189dc9d4c7f7aa64515803a78b98d9664e30d2a
Konrad Cinkusz <konradcinkusz@gmail.com>
2020-02-19 10:44:22 +0100

    Chp. 6

--- EF Entity Framework/SportsStore/SportsStore/Models/DataRepository.cs
+++ EF Entity Framework/SportsStore/SportsStore/Models/DataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.Models
 {
@@ -10,11 +11,44 @@ namespace SportsStore.Models
         {
             context = ctx;
         }
-        public IEnumerable<Product> Products => context.Products;
+        public IEnumerable<Product> Products => context.Products.ToArray();
+        public Product GetProduct(long key) => context.Products.Find(key);
         public void AddProduct(Product product)
         {
             this.context.Products.Add(product);
             this.context.SaveChanges();
+        }
+        public void UpdateProduct(Product product)
+        {
+            Product p = GetProduct(product.Id);
+            p.Name = product.Name;
+            p.Category = product.Category;
+            p.PurchasePrice = product.PurchasePrice;
+            p.RetailPrice = product.RetailPrice;
+            //context.Products.Update(product);
+            context.SaveChanges();
+        }
+        public void UpdateAll(Product[] products)
+        {
+            //context.Products.UpdateRange(products);
+            Dictionary<long, Product> data = products.ToDictionary(p => p.Id);
+            IEnumerable<Product> baseline = context.Products.Where(p => data.Keys.Contains(p.Id));
+
+            foreach (Product databaseProduct in baseline)
+            {
+                Product requestProduct = data[databaseProduct.Id];
+                databaseProduct.Name = requestProduct.Name;
+                databaseProduct.Category = requestProduct.Category;
+                databaseProduct.PurchasePrice = requestProduct.PurchasePrice;
+                databaseProduct.RetailPrice = requestProduct.RetailPrice;
+            }
+
+            context.SaveChanges();
+        }
+        public void Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
         }
     }
 }
--- EF Entity Framework/SportsStore/SportsStore/Models/IRepository.cs
+++ EF Entity Framework/SportsStore/SportsStore/Models/IRepository.cs
@@ -5,6 +5,10 @@ namespace SportsStore.Models
     public interface IRepository
     {
         IEnumerable<Product> Products { get;}
+        Product GetProduct(long key);
         void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void UpdateAll(Product[] products);
+        void Delete(Product product);
     }
 }
